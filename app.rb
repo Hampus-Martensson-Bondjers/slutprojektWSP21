1d5ed30d792b1129e5ee1d6f783c8ead1959dbc2
Hampus-Martensson-Bondjers <hampus.martenssonbondjers@elev.ga.ntig.se>
2021-04-22 19:52:06 +0200

    dvfsdfsd

--- app.rb
+++ app.rb
@@ -4,3 +4,26 @@ require 'sqlite3'
 require 'bcrypt'
 enable :sessions
 
+
+
+
+get('/') do
+    slim(:register)
+end
+
+post('/users/new') do
+    username = params[:username]
+    password = params[:password]
+    confirm_password = params[:confirm_password]
+
+    if (password == confirm_password)
+        #Lägg till användaren
+        password_digest = BCrypt::Password.create(password)
+        db = SQLite3::Databse.new('db/youtuberipoff.db')
+        db.execute("INSERT INTO users (username,pwdigest) VALUES (?,?)",username,password_digest)
+        redirect('/')
+    else
+        #felhantering
+        'Du skrev in fel lösenord'
+    end
+end
